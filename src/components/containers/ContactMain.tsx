da707884af21d857e7af2ba7c2ec2745613b1908
Abhishek <abhishekverman3459@gmail.com>
2024-02-22 12:26:47 +0530

    adding finnal Change

---
+++ src/components/containers/ContactMain.tsx
@@ -0,0 +1,149 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import phone from "public/images/phone.png";
+import mail from "public/images/mail.png";
+import location from "public/images/location.png";
+import time from "public/images/time.png";
+
+const ContactMain = () => {
+  return (
+    <section className="section contact-m fade-wrapper">
+      <div className="container">
+        <div className="row gaper">
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="contact-m__single topy-tilt fade-top">
+              <div className="thumb">
+                <Image src={phone} alt="Image" />
+              </div>
+              <div className="content">
+                <h4>Phone </h4>
+                <p>
+                  <Link href="tel:197-90-56-780">Mobile :+91 72609 20637</Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="contact-m__single topy-tilt fade-top">
+              <div className="thumb">
+                <Image src={mail} alt="Image" />
+              </div>
+              <div className="content">
+                <h4>Mail Address</h4>
+                <p>
+                  <Link href="mailto:unblueconsultation@gmail.com">
+                    unblueconsultation@gmail.com
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="contact-m__single topy-tilt fade-top">
+              <div className="thumb">
+                <Image src={location} alt="Image" />
+              </div>
+              <div className="content">
+                <h4>Our Location</h4>
+                <p>
+                  <Link
+                    href=""
+                    target="_blank"
+                  >
+                    Haldia, West Bengal,India
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="contact-m__single topy-tilt fade-top">
+              <div className="thumb">
+                <Image src={time} alt="Image" />
+              </div>
+              <div className="content">
+                <h4>Office Hour</h4>
+                <p>Sun - Thu 09 am - 06pm</p>
+                <p>Fri - Sat 4 pm - 10pm</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="map-wrapper">
+              <div className="row gaper">
+                <div className="col-12 col-lg-6">
+                  <div className="contact__map fade-top">
+                    <iframe
+                      src=""
+                      width="100"
+                      height="800"
+                      style={{ border: "0px" }}
+                      allowFullScreen
+                      loading="lazy"
+                      referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  </div>
+                </div>
+                <div className="col-12 col-lg-6">
+                  <div className="contact-main__form  fade-top">
+                    <h3>Leave A Message</h3>
+                    <form
+                      action="#"
+                      method="post"
+                      className="section__content-cta"
+                    >
+                      <div className="group-wrapper">
+                        <div className="group-input ">
+                          <input
+                            type="text"
+                            name="contact-name"
+                            id="contactName"
+                            placeholder="Name"
+                          />
+                        </div>
+                        <div className="group-input ">
+                          <input
+                            type="email"
+                            name="contact-email"
+                            id="contactEmail"
+                            placeholder="Email"
+                          />
+                        </div>
+                      </div>
+                      <div className="group-input drt">
+                        <select className="subject">
+                          <option data-display="Subject">Subject</option>
+                          <option value="1">Account</option>
+                          <option value="2">Service</option>
+                          <option value="3">Pricing</option>
+                          <option value="4">Support</option>
+                        </select>
+                      </div>
+                      <div className="group-input ">
+                        <textarea
+                          name="contact-message"
+                          id="contactMessage"
+                          placeholder="Message"
+                        ></textarea>
+                      </div>
+                      <div className="form-cta justify-content-start">
+                        <button type="submit" className="btn btn--primary">
+                          Send Message
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactMain;
